a69284978148d47b0cb4620079c5c7f88c6c90ea
Tycho Luyben <tycho.luyben@scramcard.com>
2018-08-29 00:40:41 +0800

    working stairs

---
+++ Smart Wall.gd
@@ -0,0 +1,19 @@
+extends KinematicBody2D
+
+# class member variables go here, for example:
+# var a = 2
+# var b = "textvar"
+var old_shape
+func _ready():
+	# Called when the node is added to the scene for the first time.
+	# Initialization here
+	old_shape = $CollisionShape2D.get_shape()
+
+func _process(delta):
+	# Called every frame. Delta is time since last frame.
+	# Update game logic here.
+	var y = get_node("../Player").position.y
+	if  y + 5 > position.y: 
+		$CollisionShape2D.disabled = true
+	else:
+		$CollisionShape2D.disabled = false
